52e05f7727fd2699d31c346638b9197ebfd11632
Christian Dupuis <cd@atomist.com>
2018-06-25 11:41:27 +0200

    Update Sdm104Application.java

--- src/main/java/com/atomist/Sdm104Application.java
+++ src/main/java/com/atomist/Sdm104Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Sdm104Application {
-   
+    
 	public static void main(String[] args) {
 		org.springframework.boot.SpringApplication.run(Sdm104Application.class, args);
 	}
