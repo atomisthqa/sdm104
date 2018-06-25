746d3affda28c28fd852e4d8266189ab8e3c9229
Christian Dupuis <cd@atomist.com>
2018-06-25 11:29:52 +0200

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/Sdm104Application.java
@@ -0,0 +1,11 @@
+package com.atomist;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdm104Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Sdm104Application.class, args);
+	}
+}
